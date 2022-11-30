f63bd0db128aa0dfe5fbc7fbae9d07cf06277b3e
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-29 21:42:52 -0500

    Added Click event handler on the  element

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -13,7 +13,15 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    // Show prompt
+    promptEvent.prompt();
+    // Reset the deferred prompt variable, it can only be used once.
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
