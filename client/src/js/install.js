e9a16520ded3e67e09ed6a2069822c5e28aa43ec
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-29 21:43:47 -0500

    Added handler for the  event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -24,5 +24,8 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('install hit')
+    window.deferredPrompt = null;
+});
